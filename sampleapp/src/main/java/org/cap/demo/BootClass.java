a632975acd71cac1322b86eee8f3763c23422632
Blacksoul-08 <hhiraska@LIN65004041.corp.capgemini.com>
2021-09-20 11:16:01 +0530

    show method added!

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -3,8 +3,12 @@ package org.cap.demo;
 public class BootClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from STS");// TODO Auto-generated method stub
+		System.out.println("Hello from STS");
+		show();// TODO Auto-generated method stub
 
+	}
+	public static void show() {
+		System.out.println("Show Method here!");
 	}
 
 }
